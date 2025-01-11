0503a211cc098fd491ba9ae8f4400d62cbbfbf0e
Dorotea Požega <dorotea.pozega@fer.unizg.hr>
2025-01-11 23:34:55 +0100

    Delete MoodTuneApp directory

--- MoodTuneApp/src/main/java/hr/fer/repository/KorisnikRepository.java
+++
@@ -1,9 +0,0 @@
-package hr.fer.repository;
-
-import hr.fer.model.Korisnik;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KorisnikRepository extends JpaRepository<Korisnik, Integer> {
-}
