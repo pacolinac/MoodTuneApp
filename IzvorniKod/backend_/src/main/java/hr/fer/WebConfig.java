ccabe80e27e835079d4244c94fe9743518d074c6
doroteapozega <dorotea.pozega@fer.unizg.hr>
2025-01-30 15:45:52 +0100

    Revert "pokusaj popravka refresha"
    This reverts commit 6a8bd8712f73fe330d869cb14f216f981e154d1e.

--- IzvorniKod/backend_/src/main/java/hr/fer/WebConfig.java
+++ IzvorniKod/backend_/src/main/java/hr/fer/WebConfig.java
@@ -2,7 +2,6 @@ package hr.fer;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,13 +14,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:\\w+}").setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/index.html");
-        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css|\\.json|\\.ico|\\.png|\\.jpg|\\.mp3)$}")
-                .setViewName("forward:/index.html");
     }
 }
