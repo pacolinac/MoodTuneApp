eff6b6dc1970e6f7619dfb65b77f42908645269b
doroteapozega <dorotea.pozega@fer.unizg.hr>
2025-01-30 17:22:52 +0100

    nova proba

--- IzvorniKod/backend_/src/main/java/hr/fer/WebConfig.java
+++ IzvorniKod/backend_/src/main/java/hr/fer/WebConfig.java
@@ -19,9 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{path:[^\\.]*}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/**/{path:[^\\.]*}")
-                .setViewName("forward:/index.html");
+        registry.addViewController("/{path:[^\\.]*}").setViewName("forward:/index.html");
+        registry.addViewController("/admin/**").setViewName("forward:/index.html");
     }
 }
