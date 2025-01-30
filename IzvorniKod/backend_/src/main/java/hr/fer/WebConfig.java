b676cd2b2743161bb264cf9fbdfea0566ce683a8
doroteapozega <dorotea.pozega@fer.unizg.hr>
2025-01-30 16:40:27 +0100

    stoti pokusaj lolll

--- IzvorniKod/backend_/src/main/java/hr/fer/WebConfig.java
+++ IzvorniKod/backend_/src/main/java/hr/fer/WebConfig.java
@@ -19,7 +19,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:\\w+}").setViewName("forward:/index.html");
         registry.addViewController("/**").setViewName("forward:/index.html");
     }
 }
